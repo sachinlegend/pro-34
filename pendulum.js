0d6c87d81a5748df5ddbf127d813e14c38315728
sachinlegend <70750103+sachinlegend@users.noreply.github.com>
2020-12-12 11:01:22 +0530

    Update pendulum.js

--- pendulum.js
+++ pendulum.js
@@ -1,27 +1,19 @@
-class Pendulum {
-    constructor(x,y) {
-        var options = {
-            restitution: 1,
-            friction: 0,
-            isStatic: true,
-            frictionAir: 0.0,
-            slop: 1,
-            inertia: Infinity
-        };
-        this.body = Bodies.rectangle(x,y,40,40,options);
-        this.x = x;
-        this.y = y;
-        World.add(world, this.body);
+class Pendulum{
+    constructor(x,y){
+      var options = {
+        restitution:1,
+        friction:0.3,
+        density:0.8
+      }
+    
+      this.body = Bodies.circle(x,y,25,options);
+      World.add(world,this.body);
     }
-    display() {
-        var angle = this.body.angle;
-        var pos = this.body.position;
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle);
-        noStroke();
-        fill("red");
-        ellipse(0, 0, 60, 60);
-        pop();
+    
+    display(){
+      var pos = this.body.position;
+      fill("pink");
+      ellipseMode(RADIUS);
+      ellipse(pos.x,pos.y,25,25)
     }
-}
+    }
